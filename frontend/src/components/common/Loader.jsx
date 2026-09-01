05d0174d99964bf054c0f965a589ea6ed25b57bc
Shesco <shedrachafeignatius@gmail.com>
2026-09-01 22:17:12 +0100

    fix: update Loader component header font weight for improved emphasis

--- frontend/src/components/common/Loader.jsx
+++ frontend/src/components/common/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Loader() {
   return (
     <div className="flex flex-col items-center justify-center mt-56 md:mt-48 bg-gray-100">
-      <h1 className="cursive text-xl font-bold md:font-light animate-pulse text-gray-400 text-center italic">
+      <h1 className="cursive text-xl font-extrabold md:font-light animate-pulse text-gray-400 text-center italic">
         - Website Construction In Progress -  <br /> Stay Tuned!</h1>
     </div>
   )
