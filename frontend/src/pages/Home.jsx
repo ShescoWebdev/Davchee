e39df7a440602db54b1cb2ec9dc6470d8e27c828
Shesco <shedrachafeignatius@gmail.com>
2026-09-01 17:31:35 +0100

    fix: adjust heading margins for About, Contact, Home, and Services pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,20 +2,9 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
-        {/* <div>
-            <img className="
-                h-20 w-auto
-                mt-20
-                items-start
-                "
-                 src="Logo1.png" 
-                 alt="Logo" />
-        </div> */}
-      <div className='items-center flex flex-col'>
-        <h1 className="mt-40 text-3xl font-bold text-gray-900">Welcome to the Home Page</h1>
-      <p className="text-lg text-gray-700">This is the home page content.</p>
-      </div>
+    <div className="flex flex-col items-center min-h-screen bg-gray-100">
+      <h1 className='mt-28 text-2xl font-bold text-gray-900'>Welcome to the Home Page</h1>
+        <p className="text-lg text-gray-700">This is the main landing page of our website.</p>
     </div>
   )
 }
